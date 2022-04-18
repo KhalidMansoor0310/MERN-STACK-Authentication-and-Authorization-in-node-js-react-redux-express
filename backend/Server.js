431b5c072e1565ee98701212395cd78734159796
Khalid Mansoor <57571973+KhalidMansoor0310@users.noreply.github.com>
2022-04-17 20:18:38 -0800

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 5000
+const port = 5000 || process.env.PORT;
 const mongoose = require('mongoose');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/user',require('./routes/userRoute'));
-mongoose.connect('mongodb+srv://khalidmansoor:Mansoor00@cluster0.yetfe.mongodb.net/article_DB?retryWrites=true&w=majority',()=>{
+mongoose.connect(process.env.PORT,()=>{
     console.log('connected to mongoDB');
 })
 
